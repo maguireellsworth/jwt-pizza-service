d4bde9a236dc78a607acc1d3b72029edbdf68605
maguire ellsworth <m3k4e@byu.edu>
2026-02-04 18:56:20 -0700

    added dummy test to see database in jest coverage

---
+++ tests/database.test.js
@@ -0,0 +1,11 @@
+const DB = require('../src/database/database');
+
+describe("database object should", () => {
+    beforeAll(() => {
+        const new_DB = DB();
+    })
+  
+    test('dummy db test', async () => {
+        
+    })
+})
